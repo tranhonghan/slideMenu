b86be5342b4842814a1f5c523671949f6aa31528
HanTH <honghan.tran@planet1world.com>
2019-10-30 20:15:05 +0700

    first commit

---
+++ slideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideMenu
+//
+//  Created by HanTH on 10/28/19.
+//  Copyright © 2019 HanTH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
